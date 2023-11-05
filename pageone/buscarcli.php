fa8c230e723ea44e53b3237bcfa88adf493d07b7
gihhc2 <gisele.kelly21@gmail.com>
2023-11-05 15:09:37 -0300

    Add files via upload

--- pageone/buscarcli.php
+++ pageone/buscarcli.php
@@ -4,7 +4,6 @@
     if(!empty($_GET['search']))
     {
         $data = $_GET['search']; 
-        echo $data;
         $sql = "SELECT * FROM tb_clientes where cpf like '%$data%'";
     }
     else {
@@ -29,7 +28,7 @@
     <b>
          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
         <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7Zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216ZM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
-        </svg> BUSCA DE CLIENTES </b>
+        </svg> ALTERAÇÃO | BUSCA DE CLIENTES </b>
     <p></p>
 </header>
     <div class = "box-search"> 
@@ -42,7 +41,7 @@
         </div>
     <p></P>
     <div class = "m-5">
-        <table border = 3 class = "table table-bg" > 
+        <table border = 1 class = "table table-bg"> 
             <thead> 
                 <tr> 
                     <th scape = "col">ID</th> 
@@ -65,6 +64,13 @@
                         echo "<td>".$user_data['data_nasc']."</td>";
                         echo "<td>".$user_data['pedido']."</td>";
                         echo "<td>".$user_data['preco']."</td>";
+                        echo "<td>
+                            <a class = 'btn btn-primary' href='edit.php?id=$user_data[id]'>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                            <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
+                            <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
+                            </a>
+                            </td>";
                         echo "</tr>";
                     }
 
--- pageone/home.php
+++ pageone/home.php
@@ -11,15 +11,14 @@
 </head>
 <body>
     <h1>O QUE VOCÊ DESEJA FAZER HOJE?</h1> 
-    <h2>SELECIONE ABAIXO E LEMBRE-SE, VOCÊ PRECISA TER CPF OU ID DO CLIENTE.</h2>
     <div class = "box"> 
         <a href = "form.php">FAZER PEDIDO</a>
         <p> </p>
         <br></br>
-        <a href = "altercad.php">MUDAR CADASTRO </a> 
+        <a href = "buscarcli.php">MUDAR CADASTRO </a> 
         <p> </p>
         <br></br>
-        <a href = "deletpedido.php">DELETAR PEDIDO </a>
+        <a href = "buscarcli.php">DELETAR PEDIDO </a>
         <p> </p>
         <br></br>
         <a href = "buscarcli.php">BUSCAR CLIENTE </a> 
